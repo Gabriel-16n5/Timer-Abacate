db7aad5bc316df36f76cc4455caf077ebe268615
Gabriel-16n5 <gabrielgranjeia@alunos.utfpr.edu.br>
2024-06-09 14:24:19 -0400

    feature: init login screen

--- app/login.tsx
+++ app/login.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
 export default function LoginScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Login</Text>
+      <Image source={require('@/assets/images/avocado.png')} style={styles.image} />
+      <Text style={styles.title}>Faça seu login para começar</Text>
       <TextInput
         style={styles.input}
-        placeholder="Username"
+        placeholder="Email"
       />
       <TextInput
         style={styles.input}
-        placeholder="Password"
+        placeholder="Senha"
         secureTextEntry
       />
-      <Button title="Login" onPress={() => { }} />
+      <View style={styles.buttonContainer}>
+        <Button title="Entrar" onPress={() => { }} color="#34A853" />
+        <TouchableOpacity onPress={() => { }} style={styles.outlinedButton}>
+          <Text style={styles.outlinedButtonText}>Criar conta</Text>
+        </TouchableOpacity>
+      </View>
+      <Text style={styles.link}>Esqueci minha senha</Text>
     </View>
   );
 }
@@ -22,13 +29,22 @@ export default function LoginScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
     padding: 16,
+    backgroundColor: '#fafff9',
+  },
+  image: {
+    width: 30,
+    height: 36,
+    marginBottom: 16,
   },
   title: {
-    fontSize: 24,
+    textAlign: 'center',
+    fontSize: 34,
     marginBottom: 24,
+    fontWeight: 700,
   },
   input: {
     width: '100%',
@@ -37,5 +53,25 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#ccc',
     borderRadius: 4,
+  },
+  buttonContainer: {
+    width: '100%',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 16,
+  },
+  outlinedButton: {
+    borderWidth: 1,
+    borderColor: '#34A853',
+    backgroundColor: 'transparent',
+    padding: 10,
+  },
+  outlinedButtonText: {
+    color: '#34A853',
+  },
+  link: {
+    color: '#007BFF',
+    marginTop: 16,
   },
 });
