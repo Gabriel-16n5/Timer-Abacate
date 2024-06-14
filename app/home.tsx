d2a47d89695b45d9d9e18140bf79481ae5c1efea
Gabriel-16n5 <gabrielgranjeia@alunos.utfpr.edu.br>
2024-06-14 16:10:13 -0400

    chore: home layout, overlay and timer

--- app/home.tsx
+++ app/home.tsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
-import { Ionicons } from '@expo/vector-icons'; // Importar Ionicons
+import { View, Text, StyleSheet, Image, TouchableOpacity, Modal } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import Slider from '@react-native-community/slider';
 
 const PomodoroTimer = () => {
     const [timeRemaining, setTimeRemaining] = useState(0);
     const [timerRunning, setTimerRunning] = useState(false);
+    const [sliderVisible, setSliderVisible] = useState(true);
 
     useEffect(() => {
         if (timerRunning && timeRemaining > 0) {
@@ -19,16 +20,21 @@ const PomodoroTimer = () => {
     }, [timerRunning, timeRemaining]);
 
     const startTimer = () => {
-        setTimerRunning(true);
+        if (timeRemaining > 0) {
+            setTimerRunning(true);
+            setSliderVisible(false);
+        }
     };
 
     const pauseTimer = () => {
         setTimerRunning(false);
+        setSliderVisible(true);
     };
 
     const resetTimer = () => {
         setTimeRemaining(0);
         setTimerRunning(false);
+        setSliderVisible(true);
     };
 
     const formatTime = (time) => {
@@ -41,27 +47,31 @@ const PomodoroTimer = () => {
         <>
         <Text style={styles.timerText}>{formatTime(timeRemaining)}</Text>
         <View style={styles.timerContainer}>
-            <Slider
-                style={{width: 200, height: 40}}
-                trackStyle={styles.track}
-                thumbImage={require('@/assets/images/avocado.png')}
-                thumbStyle={styles.thumb}
-                minimumTrackTintColor='#a7c99a'
-                thumbTouchSize={{ width: 50, height: 40 }}
-                minimumValue={0}
-                maximumValue={12}
-                step={1}
-                onValueChange={(value) => setTimeRemaining(value * 5 * 60)}
-            />
-        <View 
-            />
+            {sliderVisible && (
+                <Slider
+                    style={{width: 200, height: 40}}
+                    trackStyle={styles.track}
+                    thumbImage={require('@/assets/images/avocado.png')}
+                    thumbStyle={styles.thumb}
+                    minimumTrackTintColor='#a7c99a'
+                    thumbTouchSize={{ width: 50, height: 40 }}
+                    minimumValue={0}
+                    maximumValue={12}
+                    step={1}
+                    onValueChange={(value) => setTimeRemaining(value * 5 * 60)}
+                />
+            )}
             {timerRunning ? (
                 <TouchableOpacity onPress={pauseTimer}>
-                    <Ionicons name="pause" size={24} color="black" />
+                    <View style={{ backgroundColor: '#89362e', borderRadius: 50, padding: 5 }}>
+                        <Ionicons name="stop" size={24} color="white" />
+                    </View>
                 </TouchableOpacity>
             ) : (
                 <TouchableOpacity onPress={startTimer}>
-                    <Ionicons name="play" size={24} color="black" />
+                    <View style={{ backgroundColor: '#89362e', borderRadius: 50, padding: 5 }}>
+                        <Ionicons name="play" size={24} color="white" />
+                    </View>
                 </TouchableOpacity>
             )}
         </View>
@@ -70,7 +80,8 @@ const PomodoroTimer = () => {
 };
 
 const AvocadoStore = () => {
-    const [avocadoMoney, setAvocadoMoney] = React.useState(0);
+    const [avocadoMoney, setAvocadoMoney] = useState(0);
+    const [modalVisible, setModalVisible] = useState(false);
 
     const handlers = () => {
         console.log('vai para loja');
@@ -78,13 +89,37 @@ const AvocadoStore = () => {
 
     return (
         <View style={styles.avocadoStoreContainer}>
-            <TouchableOpacity style={styles.avocadoContainer} onPress={handlers}>
+            <TouchableOpacity style={styles.avocadoContainer} onPress={() => setModalVisible(true)}>
                 <Image source={require('@/assets/images/avocado.png')} style={styles.image} />
                 <Text style={styles.avocadoText}>{avocadoMoney}</Text>
             </TouchableOpacity>
+
+            <Modal
+                animationType="slide"
+                transparent={false}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    setModalVisible(!modalVisible);
+                }}
+            >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <TouchableOpacity
+                            style={[styles.buttonClose, {alignSelf: 'flex-start'}]}
+                            onPress={() => setModalVisible(!modalVisible)}
+                        >
+                            <Text style={styles.textStyle}>X</Text>
+                        </TouchableOpacity>
+                        <Text style={styles.modalText}>Para que servem os abacates?</Text>
+                        <Text style={styles.modalTextBody}>Além de fazer bem e deixar o cabelo bonito, os abacates servem como moedas, ou seja, a cada timer completo você ganha um, porém se nao terminar vai perder abacate bem.</Text>
+                        <Image source={require('@/assets/images/Abacates.png')} style={styles.imageOverlay} />
+                    </View>
+                </View>
+            </Modal>
         </View>
     );
 };
+
 
 export default function SplashScreen() {
     return (
@@ -101,7 +136,6 @@ const styles = StyleSheet.create({
         height: 1,
         borderRadius: 100,
         backgroundColor: '#A7C99A',
-
     },
     thumb: {
         width: 32,
@@ -123,13 +157,17 @@ const styles = StyleSheet.create({
         color: '#06150080',
     },
     timerText: {
-        fontSize: 20,
-        fontWeight: 'bold',
+        fontSize: 58,
+        fontWeight: 300,
+        marginTop: 80,
         marginBottom: 10,
+        fontFamily: 'Open Sans',
+        color: '#1B3810'
     },
     imageJuju: {
         width: 210,
         height: 240,
+        marginTop: 100
     },
     avocadoStoreContainer: {
         position: 'absolute',
@@ -166,5 +204,58 @@ const styles = StyleSheet.create({
         backgroundColor: '#fafff9',
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    modalView: {
+        width: '100%',
+        height: '100%',
+        backgroundColor: "#defbd9",
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+    },
+    button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2
+    },
+    buttonClose: {
+        backgroundColor: "#2196F3",
+    },
+    textStyle: {
+        
+    },
+    modalText: {
+        marginBottom: 15,
+        marginLeft: 20,
+        fontSize: 30,
+        fontWeight: '700',
+        color: '#1F371B',
+        fontFamily: 'Nunito Sans',
+    },
+    modalTextBody: {
+        marginBottom: 15,
+        marginLeft: 20,
+        fontSize: 18,
+        fontWeight: '400',
+        color: '#1F371B',
+        fontFamily: 'Open Sans',
+    },
+    imageOverlay: {
+        width: 380,
+        height: 228,
+        position: 'absolute',
+        bottom: 0
     },
 });
